6de882dab8f0e3ca65b9250272b059f499378654
devbg21 <31804288+bg21@users.noreply.github.com>
2023-11-04 00:11:49 -0300

    Update file.js

--- js/file.js
+++ js/file.js
@@ -1,9 +1,10 @@
-const textarea = document.getElementById("textarea");
-const restante = document.getElementById("restante");
-const maximo = 250;
+window.addEventListener('load', () => {
+	const textarea = document.getElementById('textarea');
+	const restante = document.getElementById('restante');
+	const maximo = 100;
 
-textarea.addEventListener('input', function(){
-    const restanteCaractere = maximo - textarea.value.length;
-
-    restante.textContent = restanteCaractere + " caracteres restantes.";
+	textarea.addEventListener('input', function () {
+		const restanteCaractere = maximo - textarea.value.length;
+		restante.textContent = restanteCaractere + ' caracteres restantes.';
+	});
 });
